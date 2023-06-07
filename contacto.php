9fb732df8f84402e4e64f078bf44389564122576
Gitnagel16 <135839648+Gitnagel16@users.noreply.github.com>
2023-06-07 13:49:50 -0300

    Actualizando o Update el primer archivo contacto.php

---
+++ contacto.php
@@ -0,0 +1,13 @@
+<?php
+
+$persona = [
+              "nombre" => "Jorge",
+              "apellido" => "Nagel",
+              "edad" => 29
+          ];
+  
+
+          foreach ($persona as $valor) {
+              echo "$valor <br>";
+          }
+?>
